32473557a66994ce13d145bad1ac44a4f997d943
pranjalup <pranjalu05@gmail.com>
2025-11-10 14:28:15 +0530

    Fix case of include_colab_link value

--- employee_salary_app.py
+++ employee_salary_app.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyM7se+5AJDnXgf+4JC2bXjd",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
